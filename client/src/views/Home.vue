90e1541db12cae38151ec5dd746f131447a517d6
joeyklee <jsphknglee@gmail.com>
2020-03-04 00:56:52 -0500

    adds content

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -16,7 +16,7 @@
           <h2 class="section__title">Who Are We</h2>
           <p
             class="section__text"
-          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem doloremque dolorem minus in ex possimus magnam unde aspernatur! Fuga nihil voluptate voluptatum consequuntur nulla autem saepe et similique? Quam, asperiores?</p>
+          ><a href="/">MoreTrees</a> is a grassroots project started by two Brooklyn residents, <a href="https://www.anzollitto.com/" target="_blank" noreferrer>Andy Anzollitto</a> and <a href="https://jk-lee.com" target="_blank" noreferrer>Joey Lee</a>. Andy is a type designer and Joey is a creative technologist. Together, they have joined forces to put their design and tech skills to work to make New York City greener, one tree at a time. </p>
         </div>
       </section>
       <section class="section section--tree">
@@ -28,7 +28,7 @@
           <h2 class="section__title">What We're Doing</h2>
           <p
             class="section__text"
-          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem doloremque dolorem minus in ex possimus magnam unde aspernatur! Fuga nihil voluptate voluptatum consequuntur nulla autem saepe et similique? Quam, asperiores?</p>
+          ><a href="/">MoreTrees</a> is a project about mapping empty tree beds across New York City. Why? By mapping the empty tree beds, we can make a <a href="https://portal.311.nyc.gov/report-problems/" noreferrer target="_blank">311 request</a> for the City to plant a tree there! Whether you're walking your dog, on your way to work, or just on a stroll, you can submit the location of an empty tree bed and we'll submit your 311 request for you!</p>
         </div>
       </section>
       <section class="section section--tree">
@@ -40,7 +40,16 @@
           <h2 class="section__title">Get Involved</h2>
           <p
             class="section__text"
-          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem doloremque dolorem minus in ex possimus magnam unde aspernatur! Fuga nihil voluptate voluptatum consequuntur nulla autem saepe et similique? Quam, asperiores?</p>
+          >You can help us by: 
+          <ol>
+            <li>Submitting locations of empty tree beds, </li>
+            <li>Maintaining existing trees where you live, and </li>
+            <li>Getting involved in the NYC tree stewardship program.</li>
+          </ol>
+          </p>
+          <p class="section__text">
+            If it is within your means, you may also consider donating to support our project or sponsoring other helpful costs like our server and database costs. Any contributions are welcome!
+          </p>
         </div>
       </section>
       <section class="section section--tree">
@@ -52,7 +61,7 @@
           <h2 class="section__title">Learn More</h2>
           <p
             class="section__text"
-          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem doloremque dolorem minus in ex possimus magnam unde aspernatur! Fuga nihil voluptate voluptatum consequuntur nulla autem saepe et similique? Quam, asperiores?</p>
+          >There's so much to learn! Are you interested in learning about trees? The human health and environmental benefits trees provide? The opportunities and initiatives offered by New York City to get involved with environmental stewardship? If so, check out our learning page! </p>
         </div>
       </section>
     </main>
@@ -131,11 +140,13 @@ export default {
     z-index: 100;
     max-width: 400px;
     width: 100%;
+    font-size:2.6rem;
   }
   &__text {
     z-index: 100;
     max-width: 400px;
     width: 100%;
+    font-size: 1.2rem;
   }
 
   &__content {
