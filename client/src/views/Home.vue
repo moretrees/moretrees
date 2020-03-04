ea425671e03a3d0e525e2e4e76428c2baf530d33
joeyklee <jsphknglee@gmail.com>
2020-03-04 00:10:07 -0500

    adds temp visual assets

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -10,12 +10,18 @@
         <h2 class="section__title">Section 1</h2>
       </section>
       <section class="section section--tree">
+        <!-- <svg class="planter__icon">
+          <use xlink:href="../assets/planter.svg" />
+        </svg> -->
+        <img class="planter__icon" src="../assets/planter.png">
         <h2 class="section__title">Section 2</h2>
       </section>
       <section class="section section--tree">
+        <img class="tree-branch__icon tree-branch__icon--right" src="../assets/tree-branch--right.png">
         <h2 class="section__title">Section 3</h2>
       </section>
       <section class="section section--tree">
+        <img class="tree-branch__icon tree-branch__icon--left" src="../assets/tree-branch--left.png">
         <h2 class="section__title">Section 4</h2>
       </section>
     </main>
@@ -25,7 +31,16 @@
 <script>
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  methods: {
+    scrollToBottom() {
+      var $header = document.querySelector(".header");
+      $header.scrollIntoView();
+    }
+  },
+  mounted() {
+    this.scrollToBottom();
+  }
 };
 </script>
 
@@ -80,7 +95,6 @@ export default {
 
 .section {
   height: 90vh;
-  border: 1px solid #eee;
 
   &--lawn {
     background: rgb(115, 144, 58);
@@ -105,6 +119,31 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+  }
+}
+.planter__icon{
+  z-index:9000;
+  width:400px;
+  height: 8rem;
+  position: absolute;
+  bottom:0;
+  left:50%;
+  transform: translateX(-50%);
+}
+.tree-branch__icon{
+  width:520px;
+  position:absolute;
+  z-index: 9000;
+
+  &--left{
+    left:50%;
+    top:50%;
+    transform: translate(-90%,-50%);
+  }
+   &--right{
+    left:50%;
+    top:50%;
+    transform: translate(-10%,-50%);
   }
 }
 </style>
